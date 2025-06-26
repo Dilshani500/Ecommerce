76753761f5a8af52b6208a7ad8def3232f237bf6
Dilshani500 <dilshaniwee50@gmail.com>
2025-06-26 17:53:08 +0530

    dinamic response

--- app/Livewire/CategoriesPage.php
+++ app/Livewire/CategoriesPage.php
@@ -3,11 +3,21 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\Attributes\Title;
+use App\Models\Category;
+
+
+
+#[Title('Categories - Ecommerce')]
 
 class CategoriesPage extends Component
 {
     public function render()
     {
-        return view('livewire.categories-page');
+
+        $Categories = Category::where('is_active', 1)->get();
+        return view('livewire.categories-page',[
+            'categories' => $Categories,
+        ]);
     }
 }
