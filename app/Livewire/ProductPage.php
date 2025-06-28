5a9cdeb0edd623692808ad4c34e23755e711bf47
Dilshani500 <dilshaniwee50@gmail.com>
2025-06-28 14:29:50 +0530

    dinamic

--- app/Livewire/ProductPage.php
+++ app/Livewire/ProductPage.php
@@ -3,11 +3,29 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\Attributes\Title;
+use App\Models\Product;
+use livewire\WithPagination;
+use App\Models\Brand;
+use App\Models\Category;
+
+
+
+
+
+
+#[Title('Products - Ecommerce')]
 
 class ProductPage extends Component
 {
+    use WithPagination;
     public function render()
     {
-        return view('livewire.product-page');
+        $productQuery = Product::query()->where('is_active',1);
+        return view('livewire.product-page',[
+            'products' => $productQuery->paginate(6),
+            'brands' => Brand::where('is_active', 1)->get(['id', 'name','slug']),
+            'categories' => Category::where('is_active', 1)->get(['id', 'name','slug'])
+        ]);
     }
 }
